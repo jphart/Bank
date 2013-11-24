ad23f520e58b7df30c067faf1089098db90993e4
Jonathan Hart <jhartuk@gmail.com>
2013-11-24 12:30:05 +0000

    Adds code coverage plugin

--- test/unit/bank/TransactionTests.groovy
+++ test/unit/bank/TransactionTests.groovy
@@ -7,13 +7,14 @@ class TransactionTests extends GrailsUnitTestCase {
     def testTransaction
     def from
     def to
+    final double amount = 5.0d
     
     protected void setUp() {
         super.setUp()
         from = new Account(name:"validName1",email:"valid1@test.com", balance: 10.0d)
         to = new Account(name:"validName2",email:"valid2@test.com", balance: 10.0d)
         
-        testTransaction = new Transaction(from: from, to:to, amount:5.0d)
+        testTransaction = new Transaction(from: from, to:to, amount:amount)
     }
 
     protected void tearDown() {
@@ -28,6 +29,11 @@ class TransactionTests extends GrailsUnitTestCase {
         
         assertEquals "nullable", transaction.errors["from"]
         assertEquals "nullable", transaction.errors["to"]
+    }
+    
+    void testToStringReturnsCorrectValues(){
+        String expected = "Payment from ${from.name} to ${to.name} for ${amount}"
+        assertEquals expected, testTransaction.toString()
     }
     
     void testNegativeAmountIsInvalid(){
